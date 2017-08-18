f263e390d0b3ab5f804d210a4519c8b3a3a4b18a
dkyryk <dkyryk@provectus.com>
2017-08-19 00:12:53 +0300

    if + list operations

--- learn/FunctionTest.hs
+++ learn/FunctionTest.hs
@@ -3,3 +3,13 @@ module FunctionTest where
 isEqual a b = a == b
 
 isZero a = a == 0
+
+findMax a b = if a >= b
+              then a
+              else b
+
+concatLists a b = a ++ b
+
+getNthElement a i = a !! i
+
+getMiddleElement a = a !! (div (length a) 2)
